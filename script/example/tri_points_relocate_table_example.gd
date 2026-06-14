f983c965cafa578b419f385b564142338b9f94af
Éloi Strée <eloistree@gmail.com>
2026-06-14 04:27:18 -0700

    Save

--- script/example/tri_points_relocate_table_example.gd
+++ script/example/tri_points_relocate_table_example.gd
@@ -5,44 +5,26 @@ extends Node
 @export var _table_to_relocate: Node3D
 
 func relocate_table(start: Vector3, middle: Vector3, end: Vector3):
-	# Project all points to the same horizontal plane (middle height)
 	start.y = middle.y
 	end.y = middle.y
-	
-	# Degenerate check
 	if start.distance_to(middle) < 0.001 or middle.distance_to(end) < 0.001 or start.distance_to(end) < 0.001:
 		_table_to_relocate.global_position = middle
 		_table_to_relocate.scale = Vector3.ONE
 		return
-
 	var center: Vector3 = get_circumcenter(start, middle, end)
-	
 	if center == Vector3.ZERO or is_nan(center.x):
 		_table_to_relocate.global_position = middle
 		_table_to_relocate.scale = Vector3.ONE
 		return
-
-	# Radius from the three points (after projection)
 	var r1 = (start - center).length()
 	var r2 = (middle - center).length()
 	var r3 = (end - center).length()
 	var radius = r2
-
 	_table_to_relocate.global_position = center
 	_table_to_relocate.scale = Vector3(radius*2.0, 0.01, radius*2.0)
-
-	# Rotate so -Z points toward middle
 	var direction = (middle - center).normalized()
 	var angle_y: float = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP)
 	_table_to_relocate.global_rotation_degrees = Vector3(0, rad_to_deg(angle_y), 0)
-
-	# Debug
-	print("=== Cylinder Placement (Horizontal) ===")
-	print("Center: ", center)
-	print("Radius used: ", radius)
-	print("Dist start:  ", r1)
-	print("Dist middle: ", r2)
-	print("Dist end:    ", r3)
 
 
 func get_circumcenter(a: Vector3, b: Vector3, c: Vector3) -> Vector3:
--- script/tri_points_xr_button_input_changed.gd
+++ script/tri_points_xr_button_input_changed.gd
@@ -16,17 +16,17 @@ func _ready() -> void:
 		controller_to_observe.button_released.connect(_released)
 
 func _pressed(name:String):
-	print("Pressed XR:", name)
+	#print("Pressed XR:", name)
 	if button_name_press_release==name:
-		print("Pressed XR FOUND:", name)
+		#print("Pressed XR FOUND:", name)
 		on_button_down.emit()
 		if not button_state:
 			on_button_state_changed.emit(true)
 	
 func _released(name:String):
-	print("Released XR:", name)
+	#print("Released XR:", name)
 	if button_name_press_release==name:
-		print("Released XR FOUND:", name)
+		#print("Released XR FOUND:", name)
 		on_button_up.emit()
 		if button_state:
 			on_button_state_changed.emit(false)
